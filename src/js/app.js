e5f0c40463992c50667e7c18de283ee95ce13d4b
Sofian Mourabit <smourabit@informationmapping.com>
2017-04-11 14:22:39 +0700

    add herointro

--- src/js/app.js
+++ src/js/app.js
@@ -3,15 +3,24 @@ const mobileMenu = document.querySelector('.mobile__menu');
 const nav = document.querySelector('.nav');
 const body = document.querySelector('.body');
 const header = document.querySelector('.header');
+const navContainer = document.querySelector('.nav__container');
+const logo = document.querySelector('.logo');
 
 mobileMenu.addEventListener('click', ()=>{
 
   nav.classList.toggle('is__visible');
   body.classList.toggle('push__left');
   header.classList.toggle('push__left');
-
 });
 window.addEventListener('scroll', function(e) {
-  console.log('test');
   console.log(window.scrollY);
+  if(window.scrollY){
+    header.classList.add('is__sticky');
+    navContainer.classList.add('sticky');
+    logo.classList.add('sticky');
+  }else if(!window.scrollY){
+    header.classList.remove('is__sticky');
+    navContainer.classList.remove('sticky');
+    logo.classList.remove('sticky');
+  }
 });
